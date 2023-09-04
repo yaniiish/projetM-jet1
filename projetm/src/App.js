094feecd6c52a8604f261d352817d42c976c9ece
yaniiish <yanis.herda14@gmail.com>
2023-09-04 20:55:56 +0200

    3 pages + couleur OK

--- projetm/src/App.js
+++ projetm/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import data1 from "./TESTP1.json"; 
+import data1 from "./data/Page1j.json"; 
+import data2 from "./data/Page2j.json"
+import data3 from "./data/Page3j.json"
 
 
 function App() {
 
+  const [searchTerm, setSearchTerm] = useState('');
 
 
   return (
-    <div className="App bg-slate-500 h-screen">
+    <div className="App bg-slate-500">
       <header className='flex justify-center items-center	flex-col'>
         <h1 className='text-3xl text-red-700 mt-10 font-bold  mb-8'>CONSIGNES DE TRI EcoDDS - par flux</h1>
-        <input className="mb-10 placeholder:italic placeholder:text-slate-400 block bg-white w-1/3 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Produits à recherché" type="text" name="recherche"/>
+        <input className="mb-10 placeholder:italic placeholder:text-slate-400 block bg-white w-1/3 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Produits à recherché" 
+        type="text" 
+        name="recherche"
+        />
       </header>
       <div className="container mx-auto">
       <table className="min-w-full table-auto">
@@ -23,21 +29,36 @@ function App() {
             <th className="px-4 py-2">Consigne ou danger particulier</th>
           </tr>
         </thead>
+
+      
         <tbody>
           {data1.map((item, index) => (
             <tr key={index}>
-              <td className="border px-4 py-2">{item["Bac de tri"]}</td>
+<td className={`border px-4 py-2`} style={{ backgroundColor: item["Couleur"] }}>{item["Bac de tri"]}</td>
+              <td className="border px-4 py-2">{item["Produits"]}</td>
+              <td className="border px-4 py-2">{item["EcoDDS ?"]}</td>
+              <td className="border px-4 py-2">{item["Consigne ou danger particulier"]}</td>
+
+            </tr>
+          ))}
+        </tbody>
+        <div className='mt-10'></div>
+        <tbody>
+          {data2.map((item, index) => (
+            <tr key={index} className=''>
+              <td className={`border px-4 py-2`} style={{ backgroundColor: item["Couleur"] }}>{item["Bac de tri"]}</td>
               <td className="border px-4 py-2">{item["Produits"]}</td>
               <td className="border px-4 py-2">{item["EcoDDS ?"]}</td>
               <td className="border px-4 py-2">{item["Consigne ou danger particulier"]}</td>
             </tr>
           ))}
         </tbody>
+
         <div className='mt-10'></div>
         <tbody>
-          {data1.map((item, index) => (
+          {data3.map((item, index) => (
             <tr key={index} className=''>
-              <td className="border px-4 py-2">{item["Bac de tri"]}</td>
+              <td className={`border px-4 py-2`} style={{ backgroundColor: item["Couleur"] }}>{item["Bac de tri"]}</td>
               <td className="border px-4 py-2">{item["Produits"]}</td>
               <td className="border px-4 py-2">{item["EcoDDS ?"]}</td>
               <td className="border px-4 py-2">{item["Consigne ou danger particulier"]}</td>
